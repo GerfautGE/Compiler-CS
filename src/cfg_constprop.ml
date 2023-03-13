963fcf686e0153073c9653138b37e46e2a1450ff
Gerf <73294402+GerfautGE@users.noreply.github.com>
2023-03-13 10:05:27 +0100

    feat: add constant propagation
     - next is to iomplement a way to pass const after Cassign

--- src/cfg_constprop.ml
+++ src/cfg_constprop.ml
@@ -10,13 +10,38 @@ open Options
 (* [simple_eval_eexpr e] evaluates an expression [e] with no variables. Raises
    an exception if the expression contains variables. *)
 let rec simple_eval_eexpr (e: expr) : int =
-   0
+   match e with
+   | Ebinop(b, e1, e2) ->( 
+    match b with
+    | Eadd -> simple_eval_eexpr e1 + simple_eval_eexpr e2
+    | Esub -> simple_eval_eexpr e1 - simple_eval_eexpr e2
+    | Emul -> simple_eval_eexpr e1 * simple_eval_eexpr e2
+    | Ediv -> simple_eval_eexpr e1 / simple_eval_eexpr e2
+    | Emod -> simple_eval_eexpr e1 mod simple_eval_eexpr e2
+    | Exor -> simple_eval_eexpr e1 lxor simple_eval_eexpr e2
+    | Eclt -> if simple_eval_eexpr e1 < simple_eval_eexpr e2 then 1 else 0
+    | Ecgt -> if simple_eval_eexpr e1 > simple_eval_eexpr e2 then 1 else 0
+    | Ecle -> if simple_eval_eexpr e1 <= simple_eval_eexpr e2 then 1 else 0
+    | Ecge -> if simple_eval_eexpr e1 >= simple_eval_eexpr e2 then 1 else 0
+    | Eceq -> if simple_eval_eexpr e1 = simple_eval_eexpr e2 then 1 else 0
+    | Ecne -> if simple_eval_eexpr e1 <> simple_eval_eexpr e2 then 1 else 0
+   )
+   | Eunop(u, e) ->(
+     match u with
+    | Eneg -> - simple_eval_eexpr e
+   )
+   | Eint i -> i
+   | Evar _ -> raise (Failure "simple_eval_eexpr: variable")
 
 (* If an expression contains variables, we cannot simply evaluate it. *)
 
 (* [has_vars e] indicates whether [e] contains variables. *)
 let rec has_vars (e: expr) =
-   true
+   match e with
+   | Ebinop(_, e1, e2) -> has_vars e1 || has_vars e2
+   | Eunop(_, e) -> has_vars e
+   | Eint _ -> false
+   | Evar _ -> true
 
 let const_prop_binop b e1 e2 =
   let e = Ebinop (b, e1, e2) in
@@ -32,10 +57,21 @@ let const_prop_unop u e =
 
 
 let rec const_prop_expr (e: expr) =
-   e
+  match e with
+  | Ebinop (b, e1, e2) ->
+    const_prop_binop b (const_prop_expr e1) (const_prop_expr e2)
+  | Eunop (u, e) ->
+    const_prop_unop u (const_prop_expr e)
+  | Eint(i) -> e
+  | Evar s -> e
 
 let constant_propagation_instr (i: cfg_node) : cfg_node =
-    i
+  match i with
+  | Cassign(s, e, i) -> Cassign(s, const_prop_expr e, i)
+  | Creturn(e) -> Creturn(const_prop_expr e)
+  | Cprint(e, i) -> Cprint(const_prop_expr e, i)
+  | Ccmp(e, i1, i2) -> Ccmp(const_prop_expr e, i1, i2)
+  | Cnop(i) -> Cnop(i)
 
 let constant_propagation_fun ({ cfgfunbody; _ } as f: cfg_fun) =
   let ht = Hashtbl.map (fun _ m ->
